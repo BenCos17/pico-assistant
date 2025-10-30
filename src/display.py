79c67c2a893ad1c48541f16f9f8e0920504d31ee
BenCos17 <mail@jarvisdiscordbot.net>
2025-10-30 10:09:23 +0000

    adds support for auto setting dst

--- src/display.py
+++ src/display.py
@@ -1,162 +1,183 @@
-import icons
 import gc
+import icons
 import jpegdec
 from picographics import PicoGraphics, DISPLAY_PICO_DISPLAY_2
 import utils
 import utime
+from ntp import is_dst
 
 class Display:
 
-  def __init__(self):
-      gc.enable()
-      self.display = PicoGraphics(display=DISPLAY_PICO_DISPLAY_2, rotate=0)
-      self.width, self.height = self.display.get_bounds()    
-      self.eraserPen = self.display.create_pen(0, 0, 0)
-      self.inactivePen = self.display.create_pen(68, 115, 158)
-      self.activePen = self.display.create_pen(253, 216, 53)
-      self.whitePen = self.display.create_pen(255, 255, 255)
-      self.display.set_backlight(0.5)
-      
-  def getHeaderFontPen(self):
-      now = utime.localtime(utime.time())
-      if now[3] >= 7 and now[3] <= 20:
-          return self.display.create_pen(225, 225, 225)          
-      else:
-          return self.display.create_pen(33, 33, 33)
+    def __init__(self):
+        gc.enable()
+        self.display = PicoGraphics(display=DISPLAY_PICO_DISPLAY_2, rotate=0)
+        self.width, self.height = self.display.get_bounds()    
+        self.eraserPen = self.display.create_pen(0, 0, 0)
+        self.inactivePen = self.display.create_pen(68, 115, 158)
+        self.activePen = self.display.create_pen(253, 216, 53)
+        self.whitePen = self.display.create_pen(255, 255, 255)
+        self.display.set_backlight(0.5)
+
+    def getHeaderFontPen(self):
+        now = utime.localtime(utime.time())
+        if now[3] >= 7 and now[3] <= 20:
+            return self.display.create_pen(225, 225, 225)          
+        else:
+            return self.display.create_pen(33, 33, 33)
     
-  def getFontPen(self):
-      now = utime.localtime(utime.time())
-      if now[3] >= 7 and now[3] <= 20:
-          return self.display.create_pen(33, 33, 33)
-      else:
-          return self.display.create_pen(225, 225, 225)
+    def getFontPen(self):
+        now = utime.localtime(utime.time())
+        if now[3] >= 7 and now[3] <= 20:
+            return self.display.create_pen(33, 33, 33)
+        else:
+            return self.display.create_pen(225, 225, 225)
         
-  def getBgPen(self):
-      now = utime.localtime(utime.time())      
-      if now[3] >= 7 and now[3] <= 20:
-          return self.display.create_pen(250, 250, 250)
-      else:
-          return self.display.create_pen(17, 17, 17)
+    def getBgPen(self):
+        now = utime.localtime(utime.time())      
+        if now[3] >= 7 and now[3] <= 20:
+            return self.display.create_pen(250, 250, 250)
+        else:
+            return self.display.create_pen(17, 17, 17)
     
-  def getHeaderPen(self):
-      now = utime.localtime(utime.time())      
-      if now[3] >= 7 and now[3] <= 20:
-          return self.display.create_pen(3, 169, 244)
-      else:
-          return self.display.create_pen(40, 74, 89)
+    def getHeaderPen(self):
+        now = utime.localtime(utime.time())      
+        if now[3] >= 7 and now[3] <= 20:
+            return self.display.create_pen(3, 169, 244)
+        else:
+            return self.display.create_pen(40, 74, 89)
         
-  def drawIcon(self, x, y, iconKey, isActive):      
-      if iconKey in icons.icons:
-          icon = icons.icons[iconKey]
-      else:
-          icon = icons.icons["default"]
-      transformed = list(self.transformIcon(x, y, icon))      
-      if isActive:
-          self.display.set_pen(self.activePen)
-      else:
-          self.display.set_pen(self.inactivePen)
-      self.display.polygon(transformed)
-      self.display.update()
-      icon = None
-      transformed = None
-      gc.collect()      
+    def drawIcon(self, x, y, iconKey, isActive):      
+        if iconKey in icons.icons:
+            icon = icons.icons[iconKey]
+        else:
+            icon = icons.icons["default"]
+        transformed = list(self.transformIcon(x, y, icon))      
+        if isActive:
+            self.display.set_pen(self.activePen)
+        else:
+            self.display.set_pen(self.inactivePen)
+        self.display.polygon(transformed)
+        self.display.update()
+        icon = None
+        transformed = None
+        gc.collect()      
 
-  def transformIcon(self, xOffset, yOffset, icon):
-      for (x, y) in icon:
-          yield (int(x + xOffset), int(y + yOffset))
+    def transformIcon(self, xOffset, yOffset, icon):
+        for (x, y) in icon:
+            yield (int(x + xOffset), int(y + yOffset))
 
-  def clear(self):
-      self.isAsleep = True
-      self.display.set_pen(self.eraserPen)
-      self.display.clear()
-      self.display.update()
-  
-  def drawBackground(self, areaName):
-      timeString = utils.getTimeString()
-      self.isAsleep = False
-      self.display.set_pen(self.getHeaderPen())
-      self.display.rectangle(0, 0, self.width, 50)
-      self.display.set_pen(self.getBgPen())
-      self.display.rectangle(0, 50, self.width, self.height - 50)
-      self.display.set_pen(self.whitePen)
-      self.display.text(areaName, self.getCentreTextPosition(areaName, 2), 20, 240, 2)
-      self.display.text(timeString, self.width - self.display.measure_text(timeString, 2) - 10, 20, 240, 2)
-      self.display.update()
+    def clear(self):
+        self.isAsleep = True
+        self.display.set_pen(self.eraserPen)
+        self.display.clear()
+        self.display.update()
 
-  def drawDevice(self, button, device, on):
-      nameWidth = self.display.measure_text(device["name"], 2)
-      stateWidth = self.display.measure_text(device["state"], 2)
-      if button == "A":
-          iX = nameWidth / 2 - 20
-          iY = 50
-          tX = 1
-          tY = 80
-          sX = 1
-          sY = 95
-      elif button == "B":
-          iX = nameWidth / 2 - 20
-          iY = self.height - 60
-          tX = 1
-          tY = self.height - 30
-          sX = 1
-          sY = self.height - 15
-      else:
-          iX = self.width - (nameWidth / 2)
-          iY = 50
-          tX = self.width - nameWidth
-          tY = 80
-          sX = self.width - stateWidth
-          sY = 95
-      self.drawIcon(iX, iY, device["icon"], on)
-      self.display.set_pen(self.getFontPen())
-      self.display.text(device["name"], tX, tY, 240, 2)
-      self.display.text(device["state"], sX, sY, 240, 2)
-      self.display.update()  
+    def drawBackground(self, areaName):
+        now = utime.localtime(utime.time())
+        dst_active = is_dst(now[0], now[1], now[2], now[3])
+        dst_label = "DST" if dst_active else "STD"
+
+        timeString = utils.getTimeString()  # just the time
+        self.isAsleep = False
+
+        # Draw header
+        self.display.set_pen(self.getHeaderPen())
+        self.display.rectangle(0, 0, self.width, 50)
+
+        # Draw background
+        self.display.set_pen(self.getBgPen())
+        self.display.rectangle(0, 50, self.width, self.height - 50)
+
+        self.display.set_pen(self.whitePen)
+
+        # Draw area name centered
+        self.display.text(areaName, self.getCentreTextPosition(areaName, 2) - 32, 20, 240, 2)
+
+        # Draw time near right side
+        time_x = self.width - self.display.measure_text(timeString, 2) - 50  # leave space for DST
+        self.display.text(timeString, time_x, 20, 240, 2)
+
+        # Draw DST/STD label at the far right
+        dst_x = self.width - self.display.measure_text(dst_label, 2) - 10
+        self.display.text(dst_label, dst_x, 20, 240, 2)
+
+        self.display.update()
+
+    def drawDevice(self, button, device, on):
+        nameWidth = self.display.measure_text(device["name"], 2)
+        stateWidth = self.display.measure_text(device["state"], 2)
+        if button == "A":
+            iX = nameWidth / 2 - 20
+            iY = 50
+            tX = 1
+            tY = 80
+            sX = 1
+            sY = 95
+        elif button == "B":
+            iX = nameWidth / 2 - 20
+            iY = self.height - 60
+            tX = 1
+            tY = self.height - 30
+            sX = 1
+            sY = self.height - 15
+        else:
+            iX = self.width - (nameWidth / 2)
+            iY = 50
+            tX = self.width - nameWidth
+            tY = 80
+            sX = self.width - stateWidth
+            sY = 95
+        self.drawIcon(iX, iY, device["icon"], on)
+        self.display.set_pen(self.getFontPen())
+        self.display.text(device["name"], tX, tY, 240, 2)
+        self.display.text(device["state"], sX, sY, 240, 2)
+        self.display.update()  
       
-  def drawChangeAreaY(self):
-      self.isAsleep = False      
-      self.display.set_pen(self.getFontPen())
-      self.display.text("Next Area", self.width - self.display.measure_text("Next Area", 2), self.height - 20, 240, 2)
-      self.display.update()
+    def drawChangeAreaY(self):
+        self.isAsleep = False      
+        self.display.set_pen(self.getFontPen())
+        self.display.text("Next Area", self.width - self.display.measure_text("Next Area", 2), self.height - 20, 240, 2)
+        self.display.update()
       
-  def renderCamera(self, areaName, imgBytes):
-      self.isAsleep = False
-      timeString = utils.getTimeString()
-      if imgBytes is not None:
-          decoder = jpegdec.JPEG(self.display)      
-          decoder.open_RAM(memoryview(imgBytes))
-          decoder.decode(0, 0, jpegdec.JPEG_SCALE_FULL)
-      else:
-          e = "Failed to get camera image"
-          self.display.set_pen(self.whitePen)
-          self.display.text(e, self.getCentreTextPosition(e), 100, 240, 2)
-          self.display.update()
-      self.display.set_pen(self.whitePen)
-      self.display.text(areaName, self.getCentreTextPosition(areaName), 200, 240, 2)
-      self.display.text(timeString, self.width - self.display.measure_text(timeString, 2) - 10, 200, 240, 2)
-      self.display.update()
+    def renderCamera(self, areaName, imgBytes):
+        self.isAsleep = False
+        timeString = utils.getTimeString()
+        if imgBytes is not None:
+            decoder = jpegdec.JPEG(self.display)      
+            decoder.open_RAM(memoryview(imgBytes))
+            decoder.decode(0, 0, jpegdec.JPEG_SCALE_FULL)
+        else:
+            e = "Failed to get camera image"
+            self.display.set_pen(self.whitePen)
+            self.display.text(e, self.getCentreTextPosition(e), 100, 240, 2)
+            self.display.update()
+        self.display.set_pen(self.whitePen)
+        self.display.text(areaName, self.getCentreTextPosition(areaName), 200, 240, 2)
+        self.display.text(timeString, self.width - self.display.measure_text(timeString, 2) - 10, 200, 240, 2)
+        self.display.update()
       
-  def renderClimate(self, climateData):
-      self.isAsleep = False
-      currentTempString = str(climateData["current_temp"]) + "°"
-      targetTempString = "(" + str(climateData["target_temp"]) + "°)"
-      incrementString = "+1°"
-      decrementString = "-1°"
-      self.display.set_pen(self.getFontPen())
-      self.display.text(currentTempString, self.getCentreTextPosition(currentTempString, 5), self.height - int(self.height / 2), 240, 5)
-      self.display.text(targetTempString, self.getCentreTextPosition(targetTempString, 4), self.height - int(self.height / 2) + 40, 240, 4)
-      self.display.text(incrementString, 1, 80, 240, 2)
-      self.display.text(decrementString, 1, self.height - 30, 240, 2)
-      self.display.text("Next Area", self.width - self.display.measure_text("Next Area", 2), self.height - 20, 240, 2)
-      self.display.update()
+    def renderClimate(self, climateData):
+        self.isAsleep = False
+        currentTempString = str(climateData["current_temp"]) + "°"
+        targetTempString = "(" + str(climateData["target_temp"]) + "°)"
+        incrementString = "+1°"
+        decrementString = "-1°"
+        self.display.set_pen(self.getFontPen())
+        self.display.text(currentTempString, self.getCentreTextPosition(currentTempString, 5), self.height - int(self.height / 2), 240, 5)
+        self.display.text(targetTempString, self.getCentreTextPosition(targetTempString, 4), self.height - int(self.height / 2) + 40, 240, 4)
+        self.display.text(incrementString, 1, 80, 240, 2)
+        self.display.text(decrementString, 1, self.height - 30, 240, 2)
+        self.display.text("Next Area", self.width - self.display.measure_text("Next Area", 2), self.height - 20, 240, 2)
+        self.display.update()
 
-  def getCentreTextPosition(self, text, scale=2):
-      return int(self.width - self.width / 2 - int(self.display.measure_text(text, scale) / 2))
+    def getCentreTextPosition(self, text, scale=2):
+        return int(self.width - self.width / 2 - int(self.display.measure_text(text, scale) / 2))
             
-  def sleep(self):
-      self.display.set_backlight(0)      
-      self.clear()
+    def sleep(self):
+        self.display.set_backlight(0)      
+        self.clear()
       
-  def wake(self):
-      self.display.set_backlight(0.5)
-      self.clear()
+    def wake(self):
+        self.display.set_backlight(0.5)
+        self.clear()
+
--- src/ntp.py
+++ src/ntp.py
@@ -4,36 +4,53 @@ import usocket
 import ustruct
 import utime
 
+def last_sunday(year, month):
+    # Find the date of the last Sunday in a given month
+    for day in range(31, 0, -1):
+        try:
+            t = utime.mktime((year, month, day, 0, 0, 0, 0, 0))
+            if utime.localtime(t)[6] == 6:
+                return day
+        except:
+            continue
+    return 31  # fallback
+
+def is_dst(year, month, day, hour=0):
+    # DST active: last Sunday in March 01:00 → last Sunday in October 01:00
+    march_sun = last_sunday(year, 3)
+    oct_sun = last_sunday(year, 10)
+    dst_start = utime.mktime((year, 3, march_sun, 1, 0, 0, 0, 0))
+    dst_end = utime.mktime((year, 10, oct_sun, 1, 0, 0, 0, 0))
+    t = utime.mktime((year, month, day, hour, 0, 0, 0, 0))
+    return dst_start <= t < dst_end
+
 def setTime():
-    ntpHost = config.ntp_host        
+    ntpHost = config.ntp_host
     query = bytearray(48)
     query[0] = 0x1B
     address = usocket.getaddrinfo(ntpHost, 123)[0][-1]
-    sock = usocket.socket(usocket.AF_INET, usocket.SOCK_DGRAM)    
+    sock = usocket.socket(usocket.AF_INET, usocket.SOCK_DGRAM)
     try:
-        sock.settimeout(1)
-        response = sock.sendto(query, address)
+        sock.settimeout(2)
+        sock.sendto(query, address)
         message = sock.recv(48)
-    except:
-        # NTP server timed out; reboot and try again
+    except Exception as e:
+        print("NTP error:", e)
         machine.reset()
     finally:
         sock.close()
+
     now = ustruct.unpack("!I", message[40:44])[0]
-    # Difference between NTP epoch & Unix epoch: (70*365 + 17)*86400
-    delta = 2208988800
-    # Flip negative/positive as we are subtracting from an offset
-    offset = config.utc_offset * -1
-    start = utime.mktime(config.dst_start)
-    end = utime.mktime(config.dst_end)
-    if config.dst_enabled and (now - delta) < start:
-        # Before DST, no shifting
-        delta = delta + (int(offset * 3600))
-    elif config.dst_enabled and (now - delta) < end:
-        # DST, subtract DST shift multiplier from offset
-        delta = delta + (int((offset * 3600)) - (int(config.dst_shift * 3600)))
-    else:
-        # After DST or DST not observed, no shifting
-        delta = delta + (int(offset * 3600))
-    local = utime.gmtime((now - delta))
-    machine.RTC().datetime((local[0], local[1], local[2], local[6]+1, local[3], local[4], local[5], 0))
+    delta = 2208988800  # NTP to Unix epoch
+    unix_time = int(now - delta)
+
+    offset_hours = int(config.utc_offset)
+    local = utime.gmtime(unix_time + offset_hours * 3600)
+    if is_dst(local[0], local[1], local[2], local[3]):
+        offset_hours += 1  # Add one hour for DST
+
+    local = utime.gmtime(unix_time + offset_hours * 3600)
+    # RTC datetime tuple format: (year, month, day, weekday, hour, minute, second, subseconds)
+    machine.RTC().datetime((local[0], local[1], local[2], local[6] + 1,
+                            local[3], local[4], local[5], 0))
+
